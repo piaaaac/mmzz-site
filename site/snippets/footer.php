439642bd3615ed53a78ede5689f8302dc3237425
Alex Piacentini <piacentini.alex@gmail.com>
2020-09-30 19:09:11 +0200

    footer and typographic style

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,16 @@
 
+	<section class="footer">
+	  <div class="container-fluid">
+	    <div class="row">
+	    	<div class="col d-flex align-items-end justify-content-start">
+	    		<p class="font-sans-l">&copy; Matilde Mozzanega 2020</p>
+	    		<a class="font-sans-l" href="">Privacy</a>
+	    	</div>
+	  	</div>
+		</div>
+	</section>
 
-    <script type="text/javascript"></script>
-  </body>
+
+  <script type="text/javascript"></script>
+</body>
 </html>
